87488db15347567ec1ad9096d40f9d668d9b41c3
Waffles <knharman@gmail.com>
2021-11-20 10:56:59 -0800

    initial commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,33 @@
 // Assignment code here
+function generatePassword() {
+  var password = ""
+  // prompts ask questions about password criteria
+  // password length - function returns number
+  var passwordLength = promptLength() 
+  // password case - function returns boolean
+  var passwordCase = promptCase()
+  // password special characters - function returns boolean
+  var passwordCharacters = promptCharacters()
+  // password numeric - function returns boolean
+  var passwordNumeric = promptNumeric()
+
+  // generate password using criteria 
+
+  return password
+}
+
+function promptLength() {
+  return Number
+}
+function promptCase() {
+  return Boolean
+}
+function promptCharacters() {
+  return Boolean
+}
+function promptNumeric () {
+  return Boolean
+}
 
 
 // Get references to the #generate element
@@ -7,9 +36,8 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  var passwordTextArea = document.querySelector("#password");
+  passwordTextArea.value = password;
 
 }
 
